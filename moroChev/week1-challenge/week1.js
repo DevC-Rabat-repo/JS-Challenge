fdfdfee3d8d97c2f5c24c9e06ad45da7eae33d52
rouessi <rouessi.mohcine@gmail.com>
2020-11-14 02:15:49 +0100

    modify comments

--- moroChev/week1-challenge/week1.js
+++ moroChev/week1-challenge/week1.js
@@ -22,8 +22,8 @@ function left_Or_Right_Shift(value, shiftDirection, amountOfBits) {
     // for the right shift operator, we divide our value by 2 'amoutOfBits' times
 
     switch (shiftDirection) {
-        case possibleShiftDirections.LEFT: return value * 2 ** amountOfBits
-        case possibleShiftDirections.RIGHT: return value * 2 ** -amountOfBits
+        case possibleShiftDirections.LEFT  : return value * 2 ** amountOfBits
+        case possibleShiftDirections.RIGHT : return value * 2 ** -amountOfBits
     }
 }
 
@@ -31,9 +31,9 @@ function left_Or_Right_Shift(value, shiftDirection, amountOfBits) {
 function logical_operators(value1, operator, value2) {
 
     switch (operator) {
-        case logicalOperators.AND: return andOperator(value1, value2)
-        case logicalOperators.OR: return orOperator(value1, value2)
-        case logicalOperators.NOT: return - value1 - 1
+        case logicalOperators.AND : return andOperator(value1, value2)
+        case logicalOperators.OR  : return orOperator(value1, value2)
+        case logicalOperators.NOT : return - value1 - 1
     }
 }
 
@@ -41,7 +41,7 @@ let andOperator = (value1, value2) => {
     let finalResult = 0
     let bitPower = 1
     while (value1 > 0 && value2 > 0) {
-        // to make sure both of the bits are active I used multiplication ( active bit means 1 )
+        // to make sure both of the bits are active ( active bit means 1 )
         finalResult += (value1 % 2) * (value2 % 2) * bitPower
         value1 = Math.trunc(value1 / 2)
         value2 = Math.trunc(value2 / 2)
@@ -54,7 +54,7 @@ let orOperator = (value1, value2) => {
     let finalResult = 0
     let bitPower = 1
     while(value1 > 0 || value2 > 0 ){
-        // to keep the active bit i used max method
+        // to make sure that only active bit is keep 
         finalResult += Math.max((value1 % 2) , (value2 % 2)) * bitPower
         value1 = Math.trunc(value1 / 2)
         value2 = Math.trunc(value2 / 2)
